6917acb5a7503d997ea4e2d466f972b4bff43c3a
Susan Eileen Fox <fox@macalester.edu>
2025-08-15 23:43:57 -0500

    Update q2Debug.py

--- q2Debug.py
+++ q2Debug.py
@@ -3,14 +3,15 @@ File: q2Debug.py
 
 This file contains the script for Question 2 of Homework 1
 
+This script starts with a list of 20 words (randomly generated English words). It loops through the words, keeping 
+two accumulator variables (aWords, and tWords). If it finds a word that has an 'a' in it, it adds it to aWords, and
+similarly for words that have 't' in them. Then it prints the final lists
+
 Fall 2025
 Author: <your name here>
 """
 # TODO: Put your name above!
 
-"""This script starts with a list of 20 words (randomly generated English words). It loops through the words, keeping 
-two accumulator variables (aWords, and tWords). If it finds a word that has an 'a' in it, it adds it to aWords, and
-similarly for words that have 't' in them. Then it prints the final lists"""
 
 # TODO: Find the six bugs in this code (one per line, for simplicity), mark each line with a comment describing the bug
 # TODO: Correct each bug so that the script works correctly at the end
